fbc547b5b36cab59ac1c0b18403e1a7ec83fda1d
laksh <lakshyam38@gmail.com>
2022-07-28 17:09:30 +0530

    Add caching for scraping results

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
-from wsgiref.simple_server import demo_app
+import json
+import redis
+from datetime import timedelta
 from fastapi import FastAPI, HTTPException
 
-from .main import search_title, get_content_advisory, get_info_by_id
+from main import search_title, get_content_advisory, get_info_by_id
 
 app = FastAPI()
+
+
+REDIS_SERVER_PORT = 6379
 
 
 @app.get("/")
@@ -19,10 +24,38 @@ def search_titles(search_query: str):
         HTTPException(status_code=500, detail="Couldn't connect to servers")
 
 
-@app.get("/advisory/{imdb_id}")
-def read_content_advisory(imdb_id: str):
+@app.get("/advisory/{tmdb_id}")
+def read_content_advisory(tmdb_id: int):
     try:
-        return {"parental_guide": get_content_advisory(imdb_id)}
+        title_info = get_info_by_id(tmdb_id=tmdb_id)
+        imdb_id = title_info["imdb_id"]
+
+        # Checking the Redis cache first
+        redis_client = redis.Redis(host="localhost", port=REDIS_SERVER_PORT, db=0)
+
+        response_body = {
+            "title": title_info["title"],
+            "id": tmdb_id,
+            "imdb_id": imdb_id,
+            "parental_guide": {},
+        }
+
+        parental_guide = redis_client.get(imdb_id)
+        if parental_guide:
+            print("Cache hit")
+            response_body["parental_guide"] = json.loads(parental_guide)
+            return response_body
+
+        print("Cache miss")
+
+        # Scraping since cache miss
+        parental_guide = get_content_advisory(imdb_id)
+
+        # Saving results in cache for quick future retrieval
+        redis_client.set(imdb_id, json.dumps(parental_guide))
+        redis_client.expire(imdb_id, timedelta(minutes=15))
+
+        return response_body
     except:
         raise HTTPException(status_code=500, detail="Unsuccessful request")
 
--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
-REDIS_SERVER_PORT = 6379
-
 
 def search_title(query: str):
     load_dotenv()
@@ -17,7 +15,6 @@ def search_title(query: str):
     URL = f"https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={query}"
 
     response = requests.get(url=URL).json()
-
     results = response["results"]
 
     return results
@@ -36,17 +33,6 @@ def get_info_by_id(tmdb_id: int):
 
 
 def get_content_advisory(imdb_id: str):
-    # Checking the Redis cache first
-    redis_client = redis.Redis(host="localhost", port=REDIS_SERVER_PORT, db=0)
-
-    result = redis_client.get(imdb_id)
-    if result:
-        print("Cache hit")
-        return json.loads(result)
-
-    print("Cache miss")
-
-    # Scraping since cache miss
     result = {}
     ADV_URL = f"https://www.imdb.com/title/{imdb_id}/parentalguide"
 
@@ -71,17 +57,6 @@ def get_content_advisory(imdb_id: str):
 
             result[c] = severity
 
-        # Saving results in cache for quick future retrieval
-        redis_client.set(imdb_id, json.dumps(result))
-        redis_client.expire(imdb_id, timedelta(minutes=15))
-
         return result
     else:
         raise Exception(f"Unsuccessful HTTP request on {ADV_URL}")
-
-
-print(
-    get_content_advisory(
-        get_info_by_id(search_title("amazing spiderman")[0]["id"])["imdb_id"]
-    )
-)
